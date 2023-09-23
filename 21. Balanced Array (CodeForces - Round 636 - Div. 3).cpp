6df0d8706c93c37f19c9112718cc203e59094c7a
Sumaiya Tarannum Noor <sumaiyatarannumnoor001@gmail.com>
2023-09-23 22:28:32 +0600

    Update 21. Balanced Array (CodeForces - Round 636 - Div. 3).cpp

--- 21. Balanced Array (CodeForces - Round 636 - Div. 3).cpp
+++ 21. Balanced Array (CodeForces - Round 636 - Div. 3).cpp
@@ -10,37 +10,38 @@ int main(){
         int n;
         cin >> n;
         vector<int>a(n);
-        int sum =0;
-        for(int i=0; i< (n/2); i++){
-            a[i] = 2 * (i+1);
+        int sum = 0;
+        for(int i=0; i<(n/2); i++){
+            a[i]=2*(i+1);
             sum += a[i];
         }
-
-        int k =0;
+        int k = 0;
         int flag = 0;
-        for(int i=(n/2); i<n; i++){
+        for(int i=n/2; i<n; i++){
             if(i == n-1){
                 if(sum%2 != 0){
                     a[i] = sum;
                     flag = 1;
                 }
             }
-            a[i] = (2*k) + 1;
-            k++;
-            sum -= a[i];
-
+            else{
+                a[i] = (2*k) + 1;
+                k++;
+                sum -= a[i];
+            }
         }
-        if(flag == 0)
+        if(flag==0)
             cout << "NO" << "\n";
         else{
             cout << "YES" << "\n";
-            for (int i=0; i<n; i++)
+            for(int i=0; i<n; i++)
                 cout << a[i] << " ";
-            cout<<"\n";
-            }
-
+            cout << "\n";
         }
-        return 0;
+
     }
+
+    return 0;
+}
 
 
