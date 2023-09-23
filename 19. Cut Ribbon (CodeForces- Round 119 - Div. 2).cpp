252b812d9a7e61578475fe3abd163efa51d989a7
Sumaiya Tarannum Noor <sumaiyatarannumnoor001@gmail.com>
2023-09-23 21:45:14 +0600

    Update 19. Cut Ribbon (CodeForces- Round 119 - Div. 2).cpp

--- 19. Cut Ribbon (CodeForces- Round 119 - Div. 2).cpp
+++ 19. Cut Ribbon (CodeForces- Round 119 - Div. 2).cpp
@@ -3,23 +3,23 @@
 using namespace std;
 
 int main(){
-    int n, a, b, c;
-    cin>>n>>a>>b>>c;
+    int n, a, b, c ;
+    while(cin>>n>>a>>b>>c){
     int ans = 0;
-    for (int x=0; x<=4000; x++){
-        for (int y=0; y<=4000; y++){
+    for(int x=0; x <= 4000; x++){
+        for(int y=0; y <= 4000; y++){
             int zc = n - (x*a + y*b);
-            if (zc<0)
+            if(zc<0)
                 break;
-            int z = (zc/(int)c);
-            if (z ==(int)z)
-                if(x==y)
-                    ans = max(ans, x+z);
-                    break;
-                ans = max(ans, x + y + z);
+            double z = (zc/(double)c);
+            if(z==(int)z)
+                ans = max(ans, (int)z+y+x);
         }
-
     }
-    cout<<ans;
+
+    cout<<ans<<endl;
     return 0;
+    }
+
 }
+
