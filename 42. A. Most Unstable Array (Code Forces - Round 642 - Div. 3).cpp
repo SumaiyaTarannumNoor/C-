f7ff775b7edc4807402a02dd6d8001ca864a214b
Sumaiya Tarannum Noor <sumaiyatarannumnoor001@gmail.com>
2023-11-28 10:00:55 +0600

    Add files via upload

---
+++ 42. A. Most Unstable Array (Code Forces - Round 642 - Div. 3).cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+#define ll   long long
+
+using namespace std;
+
+int main(){
+
+    int t;
+    cin >> t;
+    while(t--){
+        ll n, m;
+        cin >> n >> m;
+        if(n == 1) cout << 0 << "\n";
+        else if(n == 2) cout << m << "\n";
+        else cout << 2LL*m << "\n";
+    }
+
+}
