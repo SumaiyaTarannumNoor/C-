2f90a5457482dd936312e8abd5fc509cf2014ae8
Sumaiya Tarannum Noor <sumaiyatarannumnoor001@gmail.com>
2024-05-02 09:31:53 +0600

    Almost 80 Yeeeeeee!!!!!!!!!!!!!!!!!!!

---
+++ 78. FashionabLee (CodeForces - Round 652 - Div. 2).cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--){
+        ll n;
+        cin >> n;
+        if(n%4)
+            cout << "NO" << "\n";
+        else
+            cout << "YES" << "\n";
+    }
+}
